6c84d7520145f99deafa995e1a3a9cd88a3b19f9
HannistHK <144600078+HannistHK@users.noreply.github.com>
2025-02-02 20:14:38 +0500

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,30 @@
+body {
+    font-family: 'Arial', sans-serif;
+    text-align: center;
+    background-color: #fbeaff;
+    color: #5e548e;
+    margin: 0;
+    padding: 0;
+}
+
+.countdown-container {
+    margin-top: 20vh;
+    padding: 20px;
+    background: #ffcbf2;
+    display: inline-block;
+    border-radius: 15px;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+    font-size: 36px;
+    font-weight: bold;
+    color: #7a5195;
+}
+
+#countdown {
+    font-size: 48px;
+    font-weight: bold;
+    margin-top: 10px;
+    color: #9f86c0;
+}
